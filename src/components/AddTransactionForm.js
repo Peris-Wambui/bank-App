6658be4e12f6c0a396a89143a0e358016b36bbb2
Peris-Wambui <wangechi.wambui@student.moringaschool.com>
2022-09-26 01:04:05 -0400

    use of array method onChange

--- src/components/AddTransactionForm.js
+++ src/components/AddTransactionForm.js
@@ -1,21 +1,28 @@
-import React from "react";
+import React, {useState} from "react";
 
 function AddTransactionForm({submit}) {
 
+  const[formTrans,setFormTrans]= useState([])
+  function handleChange(e){
+    e.preventDefult();
+    setFormTrans({
+    ...formData,
+  });}
   function getSubmitTrans(e){
     e.preventDefult();
-    submit()
+
+    // submit()
   }
   return (
     <div className="ui segment">
-      <form className="ui form" onSubmit={()=>submit()}>
+      <form className="ui form" >
         <div className="inline fields">
-          <input type="date" name="date" />
-          <input type="text" name="description" placeholder="Description" />
-          <input type="text" name="category" placeholder="Category" />
-          <input type="number" name="amount" placeholder="Amount" step="0.01" />
+          <input type="date" name="date" onChange={handleChange}/>
+          <input type="text" name="description" placeholder="Description" onChange={handleChange} />
+          <input type="text" name="category" placeholder="Category" onChange={handleChange} />
+          <input type="number" name="amount" placeholder="Amount" step="0.01" onChange={handleChange}/>
         </div>
-        <button className="ui button" type="submit">
+        <button className="ui button" type="submit" onClick={getSubmitTrans}>
           Add Transaction
         </button>
       </form>

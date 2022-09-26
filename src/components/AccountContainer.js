2f254737e6b0d191cd2b61b1e611e77615cf48ad
Peris-Wambui <wangechi.wambui@student.moringaschool.com>
2022-09-25 21:07:42 -0400

    use of useState to cause rerender

--- src/components/AccountContainer.js
+++ src/components/AccountContainer.js
@@ -2,16 +2,21 @@ import React from "react";
 import TransactionsList from "./TransactionsList";
 import Search from "./Search";
 import AddTransactionForm from "./AddTransactionForm";
+// import { useState } from "react/cjs/react.production.min";
 
 function AccountContainer() {
+  const [transactions, settransactions]=React.useState([]);
   fetch("http://localhost:8001/transactions")
   .then(resp=>resp.json())
-  .then(result=>result)
+  .then(result=>settransactions (result),[])
+  // console.log(result);
   return (
     <div>
       <Search />
       <AddTransactionForm />
-      <TransactionsList />
+      <TransactionsList  
+        Transactions={transactions}
+      />
     </div>
   );
 }
